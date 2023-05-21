f9badc4bcbdca2ef6e3a46406ef521c1f1ff64df
hyeon <115200867+jihyeon1025@users.noreply.github.com>
2023-05-21 13:20:00 +0900

    Update ProductMapper.java

--- src/main/java/com/example/moodmix/mapper/ProductMapper.java
+++ src/main/java/com/example/moodmix/mapper/ProductMapper.java
@@ -4,5 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface ProductMapper {
-    List<Map<String, Object>> getInfo(Map<String, Object> dataParam);
+    List<Map<String, Object>> getAllProducts(Map<String, Object> dataParam);
+    int getCount(String category);
+    int getTotalCount();
+    List<Map<String, Object>> getProductBySort(Map<String, Object> dataParam);
 }
