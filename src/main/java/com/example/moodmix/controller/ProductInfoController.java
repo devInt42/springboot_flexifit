5620c10ad3fb6911c32c811cb6e2312db3e586e5
hyeon <115200867+jihyeon1025@users.noreply.github.com>
2023-05-21 13:18:56 +0900

    Update ProductInfoController.java

--- src/main/java/com/example/moodmix/controller/ProductInfoController.java
+++ src/main/java/com/example/moodmix/controller/ProductInfoController.java
@@ -4,10 +4,7 @@ import com.example.moodmix.model.APIResult;
 import com.example.moodmix.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,16 +18,33 @@ public class ProductInfoController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/shirts")
-    public APIResult getProductInfo(@RequestBody Map<String, Object> param, HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping("/count")
+    public int getCount(@RequestParam(value= "category", required = false) String category) {
+        int count;
 
+        if (category == null || category.isEmpty()) {
+            count = productService.getTotalCount();
+        } else {
+            count = productService.getCountByCategory(category);
+        }
+        return count;
+    }
+
+    @PostMapping("/sort")
+    public APIResult getProductBySort(@RequestBody Map<String, Object> param, HttpServletRequest request, HttpServletResponse response) {
         APIResult result = new APIResult();
 
         Map<String, Object> dataParam = (Map<String, Object>) param.get("data");
-        APIResult res = productService.getInfo(dataParam);
-        List<Map<String, Object>> resData = (List<Map<String, Object>>) res.getResultData();
-        result.setResultData(resData);
 
+        if (dataParam == null || dataParam.isEmpty() || dataParam.get("category").equals("all")) {
+            APIResult res = productService.getAllProducts(dataParam);
+            List<Map<String, Object>> resData = (List<Map<String, Object>>) res.getResultData();
+            result.setResultData(resData);
+        }else {
+            APIResult res = productService.getProductBySort(dataParam);
+            List<Map<String, Object>> resData = (List<Map<String, Object>>) res.getResultData();
+            result.setResultData(resData);
+        }
         return result;
     }
 }
